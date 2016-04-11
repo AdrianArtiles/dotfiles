0a96603469c2e4aaf3ee0173549d08a56b51152e
Adrian Artiles <adrian.r.artiles@gmail.com>
2016-04-11 14:19:11 -0700

    updating how we now get ngrok. adding nodeboostrap. adding speedtest_cli

--- install_dev.sh
+++ install_dev.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
+brew tap caskroom/cask
 mkdir ~/Projects
 
 # Basic Tools
 brew install go
 brew install tree
 brew install watch
-brew install ngrok
 brew install fzf
 /usr/local/opt/fzf/install
+brew cask install ngrok
+
+# Non-essential Tools
+brew install speedtest_cli
 
 # Chosen Technologies
 brew install terraform
 brew install ansible
+
+# Global Libraries
+npm install nodebootstrap -g
