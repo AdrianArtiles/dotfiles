375fb907d353e060f852b0f826123371f0375ef7
Adrian Artiles <adrian.r.artiles@gmail.com>
2017-12-04 13:54:29 -0800

    installing nvm differently to make versioning easier

--- install_dev.sh
+++ install_dev.sh
@@ -14,7 +14,9 @@ brew install aspell
 /usr/local/opt/fzf/install
 brew cask install ngrok
 sudo easy_install pygments
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+git clone https://github.com/creationix/nvm.git ~/.nvm
+cd ~/.nvm && git checkout v0.33.6
 
 # Non-essential Tools
 brew cask install atom
