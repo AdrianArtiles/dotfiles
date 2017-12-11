9b8c74ccaa2970cae56b8d5bfdab48b88591f784
Adrian Artiles <adrian.r.artiles@gmail.com>
2017-12-10 23:44:41 -0800

    updating cask name from google-drive to google-backup-and-sync

--- install_user.sh
+++ install_user.sh
@@ -3,7 +3,7 @@
 brew tap caskroom/cask
 
 brew cask install google-chrome
-brew cask install google-drive
+brew cask install google-backup-and-sync
 brew cask install dropbox
 brew cask install iterm2
 brew cask install numi
