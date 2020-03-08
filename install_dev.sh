a2bbef972a0a86abf392e7a728f968eb8608df78
Adrian Artiles <adrian.r.artiles@gmail.com>
2020-03-08 12:22:56 -0700

    quick update of recent tools used

--- install_dev.sh
+++ install_dev.sh
@@ -4,22 +4,23 @@ brew tap caskroom/cask
 mkdir ~/Projects
 
 # Basic Tools
+brew install neovim
 brew install yarn
 brew install go
 brew install tree
 brew install watch
 brew install gpg
 brew install fzf
-brew install aspell
 brew install jq
+brew install wifi-password
 /usr/local/opt/fzf/install
 brew cask install ngrok
 sudo easy_install pygments
 
 # Non-essential Tools
 brew install speedtest_cli
-brew cask install postico
-brew cask install robo-3t
+brew cask install visual-studio-code
+code --install-extension vscodevim.vim
 
 # Chosen Technologies
 brew install terraform
@@ -27,7 +28,6 @@ brew install mongodb
 
 # Vendors and Platforms
 brew install hub
-brew install kops
 brew cask install google-cloud-sdk
 brew install awscli
 
--- install_user.sh
+++ install_user.sh
@@ -7,7 +7,6 @@ brew cask install google-backup-and-sync
 brew cask install google-drive-file-stream
 brew cask install dropbox
 brew cask install iterm2
-brew cask install numi
 brew cask install firefox
 brew cask install slack
 brew cask install spotify
@@ -20,10 +19,11 @@ brew cask install caffeine
 brew cask install docker
 brew cask install torbrowser
 brew cask install tunnelblick
+# brew cask install numi # setapp
+# brew cask install bartender # setapp
 brew cask install setapp
 brew cask install calibre
 brew cask install rescuetime
-brew cask install bartender
 brew cask install alfred
 brew cask install contexts
 brew cask install duet
@@ -32,10 +32,7 @@ brew cask install veracrypt
 brew install rclone
 brew cask install rclone-browser
 brew cask install keybase
-brew cask install station
 brew cask install dash
 brew cask install electrum
-brew cask install mist
 brew cask install imageoptim
-brew cask install disk-inventory-x
-brew cask install bettertouchtool
+brew cask install omnidisksweeper
