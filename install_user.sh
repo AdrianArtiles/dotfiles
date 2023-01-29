3f403eb4776e6f36de4439b4bc6ebd74bfc87d18
Adrian Artiles <adrian.r.artiles@gmail.com>
2023-01-28 21:33:04 -0800

    feat: updating google apps, removing unused, adding trials

--- install_user.sh
+++ install_user.sh
@@ -3,8 +3,8 @@
 brew tap caskroom/cask
 
 brew install --cask google-chrome
-brew install --cask google-backup-and-sync
-brew install --cask google-drive-file-stream
+brew install --cask google-earth-pro
+brew install --cask google-drive
 brew install --cask dropbox
 brew install --cask iterm2
 brew install --cask firefox
@@ -19,24 +19,22 @@ brew install --cask vlc
 brew install --cask caffeine
 brew install --cask hyperdock
 brew install --cask docker
+brew install --cask discord
 brew install --cask tor-browser
 brew install --cask tunnelblick
 brew install --cask setapp
 brew install --cask calibre
-brew install --cask rescuetime
+brew install --cask audacity
 brew install --cask alfred
+brew install --cask raycast
 brew install --cask contexts
-brew install --cask duet
 brew install --cask microsoft-outlook
 brew install --cask microsoft-excel
-brew install --cask grammarly
 brew install --cask macdown
 brew install --cask veracrypt
 brew install rclone
-brew install --cask keybase
 brew install --cask steam
 brew install --cask electrum
 brew install --cask imageoptim
-brew install --cask omnidisksweeper
-brew install --cask linphone
+# brew install --cask omnidisksweeper
 brew install --cask shadow
