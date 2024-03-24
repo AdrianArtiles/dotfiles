8e480496fff401926d8b31717e059cd215e1a7eb
Adrian Artiles <adrian.r.artiles@gmail.com>
2024-03-24 00:12:02 -0700

    feat: removing unused tools, some refactoring

--- install_dev.sh
+++ install_dev.sh
@@ -1,71 +1,55 @@
 #!/bin/sh
 
 brew tap caskroom/cask
-brew tap elastic/tap
 brew tap homebrew/cask-fonts
-brew tap oven-sh/bun
-brew tap hashicorp/tap
-mkdir ~/Projects
 
 # Basic Tools
 brew install neovim
-# brew install yarn
-brew install bun
 brew install pnpm
+brew install oven-sh/bun/bun
+brew install zsh-history-substring-search
 brew install go
 brew install tree
 brew install watch
 brew install gpg
 brew install fzf
 brew install ripgrep
-brew install fasd
 brew install fd
 brew install jq
 brew install fx
 brew install bat
 brew install exa
 brew install cloc
-brew install qrencode
-brew install wifi-password
+brew install zoxide
 brew install ffmpeg
 brew install shred
-brew install http-server
-brew install git-gui
-brew install --cask ngrok
 brew install ipfs
 brew install p7zip
+brew install imagemagick
+brew install midnight-commander
+brew install btop
+brew install htop
+brew install tlrc
 
-$(brew --prefix)/opt/fzf/install
-
-# Install Neo/Vim Plugin Manager
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Non-essential Tools
-brew install pjproject
+# Additional Tools
+# brew install pjproject
 brew install speedtest_cli
 brew install --cask visual-studio-code
-brew install --cask runjs
-brew install --cask altair-graphql-client
-brew install --cask http-toolkit
 brew install --cask charles
 brew install mitmproxy
-# brew install --cask another-redis-desktop-manager
+brew install http-server
+brew install --cask ngrok
 brew install --cask redisinsight
 brew install --cask font-jetbrains-mono-nerd-font
 code --install-extension vscodevim.vim
 brew install --cask cursor
 brew install clamav
-
-# Chosen Technologies
-brew install neovim
+brew install mailsy
+brew install wifi-password
+brew install qrencode
+brew install --cask insomnia
 brew install hasura-cli
 brew install hashicorp/tap/terraform
-# brew install mongodb
-# brew install elastic/tap/elasticsearch-full
-# brew install elastic/tap/kibana-full
 
 # Vendors and Platforms
 brew install gh
@@ -77,8 +61,3 @@ brew install rover
 brew install stripe/stripe-cli/stripe
 brew install railway
 brew install flyctl
-
-# Global Libraries
-npm install -g live-server
-npm install -g tldr
-npm install -g eslint
