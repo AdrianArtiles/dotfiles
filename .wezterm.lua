17823bdbba3498663933237aea99ea89d5560d56
Adrian Artiles <adrian.r.artiles@gmail.com>
2024-06-28 21:29:16 -0700

    feat: initial dynamic terminal resizing

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 local wezterm = require 'wezterm'
-local act = wezterm.action
+-- local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.enable_scroll_bar = false
@@ -30,5 +30,56 @@ config.mouse_bindings = {
   -- },
   -- NOTE that binding only the 'Up' event can give unexpected behaviors.
 }
+
+-- config.window_padding = {
+--   left = 0,
+--   right = 0,
+--   top = 0,
+--   bottom = 0,
+-- }
+
+-- local function dynamic_padding(window, pane)
+--   -- Detect if Neovim is running by inspecting the foreground process name or arguments
+--   local process = pane:get_foreground_process_info()
+--   if process and string.find(process.name, "nvim") then
+--     -- Neovim is running, remove padding
+--     -- return { left = 0, right = 0, top = 0, bottom = 0 }
+--     config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+--   else
+--     -- Neovim is not running, use default padding
+--     -- return { left = 5, right = 5, top = 5, bottom = 5 }
+--     config.window_padding = { left = 5, right = 5, top = 5, bottom = 5 }
+--   end
+-- end
+
+local function dynamic_padding(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  -- if not overrides.harfbuzz_features then
+  --   -- If we haven't overridden it yet, then override with ligatures disabled
+  --   overrides.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+  -- else
+  --   -- else we did already, and we should disable out override now
+  --   overrides.harfbuzz_features = nil
+  -- end
+  -- overrides.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
+  local process = pane:get_foreground_process_info()
+  if process and (string.find(process.name, "nvim") or string.find(process.name, "tmux")) then
+    -- Neovim is running, remove padding
+    -- return { left = 0, right = 0, top = 0, bottom = 0 }
+    -- config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+    overrides.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+  else
+    -- Neovim is not running, use default padding
+    -- return { left = 5, right = 5, top = 5, bottom = 5 }
+    -- overrides.window_padding = { left = 5, right = 5, top = 5, bottom = 5 }
+    -- overrides.window_padding = { left = 20, right = 20, top = 20, bottom = 20 }
+    overrides.window_padding = {  }
+    -- overrides.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+  end
+
+  window:set_config_overrides(overrides)
+end
+wezterm.on("update-right-status", dynamic_padding)
 
 return config
