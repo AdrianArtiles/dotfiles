3fc3582b6da1b8dfc97c08c6dd2a7b18ef6a71d4
Adrian Artiles <adrian.r.artiles@gmail.com>
2025-04-25 23:01:00 -0700

    feat: adding yazi neovim plugin

--- .config/nvim/lua/plugins/base.lua
+++ .config/nvim/lua/plugins/base.lua
@@ -29,6 +29,32 @@ return {
     },
   },
   {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "folke/snacks.nvim"
+    },
+    keys = {
+      {
+        "<leader>-",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+  },
+  {
     "olimorris/codecompanion.nvim",
     opts = {},
     dependencies = {
