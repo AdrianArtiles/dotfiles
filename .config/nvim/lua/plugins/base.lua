94a60ef90c660a14f3ba337bb5c474e82a24eecf
Adrian Artiles <adrian.r.artiles@gmail.com>
2025-07-31 17:39:19 -0700

    feat: removing unneeded MCP tools and comments

--- .config/nvim/lua/plugins/base.lua
+++ .config/nvim/lua/plugins/base.lua
@@ -1,15 +1,5 @@
 return {
   { "AndrewRadev/splitjoin.vim" },
-  -- { "tpope/vim-dadbod" },
-  -- { "kristijanhusak/vim-dadbod-completion" },
-  -- { "kristijanhusak/vim-dadbod-ui" },
-  -- { "michaeljsmith/vim-indent-object" },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "tokyonight-night",
-  --   },
-  -- },
   {
     "zbirenbaum/copilot.lua",
     opts = {
@@ -24,7 +14,6 @@ return {
       },
       keymap = {
         preset = "default",
-        -- preset = "super-tab",
       },
     },
   },
@@ -66,13 +55,6 @@ return {
         strategies = {
           chat = {
             adapter = "copilot",
-            tools = {
-              ["mcp"] = {
-                -- calling it in a function would prevent mcphub from being loaded before it's needed
-                callback = function() return require("mcphub.extensions.codecompanion") end,
-                description = "Call tools and resources from the MCP Servers",
-              }
-            }
           },
           inline = {
             adapter = "copilot",
@@ -88,7 +70,7 @@ return {
               },
               schema = {
                 model = {
-                  default = "google/gemini-2.5-pro-preview-03-25",
+                  default = "anthropic/claude-sonnet-4",
                 },
               },
             })
@@ -98,27 +80,6 @@ return {
     end,
   },
   {
-    "ravitemer/mcphub.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",  -- Required for Job and HTTP requests
     },
-    -- comment the following line to ensure hub will be ready at the earliest
-    cmd = "MCPHub",  -- lazy load by default
-    -- build = "npm install -g mcp-hub@latest",  -- Installs required mcp-hub npm module
-    -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
-    build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
-    config = function()
-      require("mcphub").setup({
-        use_bundled_binary = true,
-        extensions = {
-          codecompanion = {
-            -- Show the mcp tool result in the chat buffer
-            show_result_in_chat = false,
-            make_vars = true, -- make chat #variables from MCP server resources
-            make_slash_commands = true, -- make /slash_commands from MCP server prompts
-          },
-        }
-      })
-    end,
   }
 }
